2b502272bd25ba345fc458428a3b98ea93b7814e
Jainil Prajapati 🪐 <github-jainil@1209.in>
2025-01-11 21:18:23 +0530

    Remove Card component and update image handling in blog posts. Deleted the Card component file and replaced traditional img tags with the Image component from 'astro:assets' in PostCard and index pages. Updated blog post image paths for consistency and improved asset management.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,8 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
+import { image } from "astro:assets";
 
 const authors = defineCollection({
-  type: 'data',
+  type: "data",
   schema: z.object({
     name: z.string(),
     avatar: z.string(),
@@ -12,18 +13,19 @@ const authors = defineCollection({
 });
 
 const blog = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.date(),
-    updatedDate: z.date().optional(),
-    image: z.string(),
-    categories: z.array(z.string()),
-    author: z.string(),
-    readingTime: z.number(),
-    featured: z.boolean().default(false),
-  }),
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.date(),
+      updatedDate: z.date().optional(),
+      image: image(),
+      categories: z.array(z.string()),
+      author: z.string(),
+      readingTime: z.number(),
+      featured: z.boolean().default(false),
+    }),
 });
 
 export const collections = { blog, authors };
